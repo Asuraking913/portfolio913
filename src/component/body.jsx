b02b6af49627c8dc3e16af5a8f883e7f7578ccdd
Israel Shedrack <israelshedrack913@gmail.com>
2024-06-09 10:51:27 -0400

    Building Responsiveness

--- src/component/body.jsx
+++ src/component/body.jsx
@@ -11,7 +11,7 @@ function Body() {
               <h1 className='poppins text-[3rem] text-[--text] line font-bold'><span><span className='sm:block'>I'm Israel</span> Shedrack</span></h1>
               <h2 className='text-2xl text-[--text] roboto font-semibold'>A Software Developer with 4years of expeience</h2>
               <p className='w-[50ch] text-[--text] roboto text-xl'>Building Responsive interfaces for websites, along with developing automated scripts for data scraping</p>
-              <button className='p-[1em] rounded-[5px] px-[1.5em] bg-[--accent] w-[40%] text-white roboto font-semibold textxl'>Download CV</button>
+              <button className='p-[1em] hover:bg-[--accentH] duration-[1s] rounded-[5px] px-[1.5em] bg-[--accent] w-[40%] text-white roboto font-semibold textxl'>Download CV</button>
             </div>
             <div className='p-[2em] pr-[8em] flex items-center justify-center pb-[4em]'>
               <img src={myImage1} className='h-[500px] rounded-br-[8em]' alt="" />
@@ -20,7 +20,7 @@ function Body() {
       <section className='h-[40vh] flex items-center justify-between px-[--padx]'>
         
         <div className='w-[50%] p-[2em]'>
-          <img src={myImage2} className='h-[500px] w-[400px] rounded-bl-[12em] rounded-br-[15em] object-cover' alt="" />
+          <img src={myImage2} className='h-[500px] rounded-bl-[4em] rounded-br-[1em] mb-[4em] object-cover' alt="" />
         </div>
         <div className='w-[60%]'>
           <h2 className='text-4xl poppins font-bold text-[--accent]'>About Me</h2>
@@ -33,7 +33,7 @@ function Body() {
             <p className='roboto text-nowrap'><span className='text-[--accent] font-bold poppins'>Contacts:</span> Facebook, Linkedin, whatsapp, email</p>
           </div>
           <div className='p-[1em] pl-0 flex items-center justify-between'>
-          <button className='p-[1em] rounded-[5px] px-[1.5em] bg-[--accent] w-[40%] text-white roboto font-semibold textxl'>Download CV</button> 
+          <button className='p-[1em] rounded-[5px] hover:bg-[--accentH] duration-[1s] px-[1.5em] bg-[--accent] w-[40%] text-white roboto font-semibold textxl'>Download CV</button> 
           <div className='flex gap-[.5em]'>
             <span className='poppins font-bold text-[--accent] text-xl'>Follow me:</span> <div className='flex gap-[.5em] items-center'>
               <i className='fa-brands text-xl fa-facebook text-blue-600'></i>
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -5,6 +5,7 @@ function Head() {
     <div className='flex justify-between fixed top-0 w-full items-center px-[--padx] py-[1em] bg-[var(--bg)]'>
         <i className='fa-solid fa-bug text-3xl text-[--accent]'></i>
         <nav>
+          <i ></i>
             <ul className='flex gap-[2.5em] text-[var(--text)]'>
                 <li className='text-[1em] items- cursor-pointer roboto font-bold uppercase'>Home</li>
                 <li className='flex items-center gap-[.5em] text-[1em] items- cursor-pointer roboto font-bold uppercase'>Projects <i className='fa-solid fa-caret-left text-[--accent]'></i></li>
